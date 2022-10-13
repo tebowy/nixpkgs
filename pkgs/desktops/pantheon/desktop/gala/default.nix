436ea2e27205cade7e878e28d5917652cee42a89
Bobby Rong <rjl931189261@126.com>
2022-10-13 08:11:35 +0800

    pantheon.gala: 6.3.1 -> 6.3.3

--- pkgs/desktops/pantheon/desktop/gala/default.nix
+++ pkgs/desktops/pantheon/desktop/gala/default.nix
@@ -26,40 +26,19 @@
 
 stdenv.mkDerivation rec {
   pname = "gala";
-  version = "6.3.1";
+  version = "6.3.3";
 
   src = fetchFromGitHub {
     owner = "elementary";
     repo = pname;
     rev = version;
-    sha256 = "sha256-7RZt6gA3wyp1cxIWBYFK+fYFSZDbjHcwYa2snOmDw1Y=";
+    sha256 = "sha256-hx9Hy7NyLZDgHY/mDWEE7z8e8VEJE2i4JdmmBWslMnk=";
   };
 
   patches = [
     # We look for plugins in `/run/current-system/sw/lib/` because
     # there are multiple plugin providers (e.g. gala and wingpanel).
     ./plugins-dir.patch
-
-    # WindowManager: save/restore easing on workspace switch
-    # https://github.com/elementary/gala/pull/1430
-    (fetchpatch {
-      url = "https://github.com/elementary/gala/commit/1f94db16c627f73af5dc69714611815e4691b5e8.patch";
-      sha256 = "sha256-PLNbAXyOG0TMn1y2QIBnL6BOQVqBA+DBgPOVJo4nDr8=";
-    })
-
-    # WindowSwitcher: fix initial alt-tab switcher indicator visibility
-    # https://github.com/elementary/gala/pull/1417
-    (fetchpatch {
-      url = "https://github.com/elementary/gala/commit/e0095415cdbfc369e6482e84b8aaffc6a04cafe7.patch";
-      sha256 = "sha256-n/BJPIrUaCQtBgDotOLq/bCAAccdbL6OwciXY115HsM=";
-    })
-
-    # MultitaskingView: fix allocation assertions
-    # https://github.com/elementary/gala/pull/1463
-    (fetchpatch {
-      url = "https://github.com/elementary/gala/commit/23c7edeb0ee9b0ff0aa48c1d19fbd1739df7af78.patch";
-      sha256 = "sha256-OfIDBfVEZoY8vMu9F8gtfRg4TYA1MUAG94BSOBKVGcI=";
-    })
 
     # Work around crash when receiving notifications
     # https://github.com/elementary/gala/pull/1497
@@ -103,6 +82,13 @@ stdenv.mkDerivation rec {
       url = "https://github.com/elementary/gala/commit/45126e4c2d3736e872c05941a2047a54788cd011.patch";
       sha256 = "sha256-LdhFFFNwvF1p1LqJXer8+DOgAptiHZHlfnQBwVEIZjo=";
     })
+
+    # Improve multitasking view animation performance
+    # https://github.com/elementary/gala/pull/1498
+    (fetchpatch {
+      url = "https://github.com/elementary/gala/commit/2d43311d7e4b28bc228dda0f428e00ea786e62bf.patch";
+      sha256 = "sha256-CzvayVOhVWLiHu6TkR/ysllMIjmu/fKox61WygeZIpc=";
+    })
   ];
 
   nativeBuildInputs = [
@@ -139,6 +125,11 @@ stdenv.mkDerivation rec {
   postPatch = ''
     chmod +x build-aux/meson/post_install.py
     patchShebangs build-aux/meson/post_install.py
+
+    # Fix PiP window being undraggable
+    # https://github.com/elementary/gala/pull/1506
+    substituteInPlace lib/DragDropAction.vala \
+      --replace "handle.reactive = false;" ""
   '';
 
   passthru = {
